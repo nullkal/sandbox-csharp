de7e55198a7f730eefeebaa7f91fba1046dd13b1
nullkal <nullkal@nil.nu>
2018-06-05 04:36:49 +0900

    Fizz Buzz

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static string FizzBuzz(int n)
+        {
+            var sb = new StringBuilder();
+
+            if (n % 3 == 0)
+            {
+                sb.Append("Fizz");
+            }
+
+            if (n % 5 == 0)
+            {
+                if (sb.Length != 0)
+                {
+                    sb.Append(" ");
+                }
+
+                sb.Append("Buzz");
+            }
+
+            string s = sb.ToString();
+            if (s.Length == 0)
+            {
+                return n.ToString();
+            } else
+            {
+                return s;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            var fizzbuzz = Enumerable.Range(1, 500).Select(FizzBuzz);
+            Console.WriteLine(String.Join(", ", fizzbuzz));
+
+            Console.ReadKey();
+        }
+    }
+}
